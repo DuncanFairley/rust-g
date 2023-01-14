ce4cf5efa40c41db0b4c5156a9505db83b9528b8
DuncanFairley <Duncan@hotdogradio.com>
2023-01-14 12:44:52 -0800

    New byond_fn format for seconds_until

--- src/time.rs
+++ src/time.rs
@@ -50,9 +50,9 @@ byond_fn!(
     }
 );
 
-byond_fn! { seconds_until(time) {
+byond_fn!(fn seconds_until(time) {
     let date_time = parse_date_string(time, Local::now(), Dialect::Us);
     let start_time = Local::now();
     let duration = date_time.unwrap() - start_time;
     Some(duration.num_seconds().to_string())
-} }
+});
